c4c761fae8ead79dc2ff289a7dea738de8f45f12
mr-possible <sambhavd4@gmail.com>
2023-03-01 16:44:35 +0000

    Added fillable fields - Book and BookReview models

--- app/Models/BookModel.php
+++ app/Models/BookModel.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class BookModel extends Model
 {
     use HasFactory;
+
+    protected $fillable=[
+        'book_title',
+        'book_author',
+        'book_description',
+        'book_isbn',
+        'book_publisher',
+        'book_pagecount',
+        'book_language',
+        'book_price'
+    ];
 }
--- app/Models/BookReviewModel.php
+++ app/Models/BookReviewModel.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class BookReviewModel extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'book_review_title',
+        'book_review_body',
+        'book_review_rating',
+        'book_id',
+        'user_id',
+        'book_review_date',
+        'book_review_upvotes',
+        'book_review_downvotes',
+    ];
 }
