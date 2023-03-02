8cb4c4b2bf2a8a8025523c4eabf629374c6226d1
mr-possible <sambhavd4@gmail.com>
2023-03-02 10:11:05 +0000

    Tested BookTableSeeder without factory seeding

--- database/seeders/BookTableSeeder.php
+++ database/seeders/BookTableSeeder.php
@@ -4,14 +4,32 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\BookModel;
 
 class BookTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
+     * Book Reference: https://www.harpercollins.com/products/the-alchemist-paulo-coelho?variant=32207222538274
      */
     public function run(): void
     {
-        //
+        $book = [
+            'book_title' => 'The Alchemist',
+            'book_author' => 'Paulo Coelho',
+            'book_description' => 'The Alchemist is a novel by Brazilian author Paulo Coelho.',
+            'book_isbn' => '978-0061122415',
+            'book_publisher' => 'HarperOne',
+            'book_pagecount' => 208,
+            'book_language' => 'English',
+            'book_price' => 12.99,
+        ];
+
+        // Book information sourced from:
+        // - Goodreads: https://www.goodreads.com/book/show/865.The_Alchemist
+        // - HarperCollins Publishers: https://www.harpercollins.com/products/the-alchemist-paulo-coelho?variant=32212166726706
+        // - Amazon: https://www.amazon.com/Alchemist-Paulo-Coelho/dp/0061122416
+
+        BookModel::create($book);
     }
 }
