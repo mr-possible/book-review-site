2c2bd5bbf20b40dacdda374e2e708c814be67b11
mr-possible <sambhavd4@gmail.com>
2023-03-02 11:10:41 +0000

    Put Factory seeding approach for BookModel

--- database/factories/BookModelFactory.php
+++ database/factories/BookModelFactory.php
@@ -16,8 +16,18 @@ class BookModelFactory extends Factory
      */
     public function definition(): array
     {
+        /**
+         * Faker Library: https://github.com/fzaninotto/Faker
+         */
         return [
-            //
+            'book_title' => fake()->sentence($nbWords = 4, $variableNbWords = true),
+            'book_author' => fake()->name,
+            'book_description' => fake()->paragraph($nbSentences = 3, $variableNbSentences = true),
+            'book_isbn' => fake()->isbn13,
+            'book_publisher' => fake()->company,
+            'book_pagecount' => fake()->numberBetween($min = 100, $max = 500),
+            'book_language' => fake()->languageCode,
+            'book_price' => fake()->randomFloat($nbMaxDecimals = 2, $min = 5, $max = 50),
         ];
     }
 }
--- database/seeders/BookTableSeeder.php
+++ database/seeders/BookTableSeeder.php
@@ -14,22 +14,30 @@ class BookTableSeeder extends Seeder
      */
     public function run(): void
     {
-        $book = [
-            'book_title' => 'The Alchemist',
-            'book_author' => 'Paulo Coelho',
-            'book_description' => 'The Alchemist is a novel by Brazilian author Paulo Coelho.',
-            'book_isbn' => '978-0061122415',
-            'book_publisher' => 'HarperOne',
-            'book_pagecount' => 208,
-            'book_language' => 'English',
-            'book_price' => 12.99,
-        ];
+        /**
+         * Using below a single instance only. 
+         */
+        // $book = [
+        //     'book_title' => 'The Alchemist',
+        //     'book_author' => 'Paulo Coelho',
+        //     'book_description' => 'The Alchemist is a novel by Brazilian author Paulo Coelho.',
+        //     'book_isbn' => '978-0061122415',
+        //     'book_publisher' => 'HarperOne',
+        //     'book_pagecount' => 208,
+        //     'book_language' => 'English',
+        //     'book_price' => 12.99,
+        // ];
 
         // Book information sourced from:
         // - Goodreads: https://www.goodreads.com/book/show/865.The_Alchemist
         // - HarperCollins Publishers: https://www.harpercollins.com/products/the-alchemist-paulo-coelho?variant=32212166726706
         // - Amazon: https://www.amazon.com/Alchemist-Paulo-Coelho/dp/0061122416
 
-        BookModel::create($book);
+        //BookModel::create($book);
+
+        /**
+         * Using factory-seeding approach below.
+         */
+        BookModel::factory()->count(20)->create();
     }
 }
