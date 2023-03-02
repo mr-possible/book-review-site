c8cb5a36aa5e1f5a0a05b82d089538c1a9718a4e
mr-possible <sambhavd4@gmail.com>
2023-03-02 11:21:48 +0000

    Put Factory seeding approach for User

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class UserTableSeeder extends Seeder
 {
@@ -12,6 +13,6 @@ class UserTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        User::factory()->count(20)->create();
     }
 }
