bb03b89fdb9de92c55df088577d009acfb2562c7
mr-possible <sambhavd4@gmail.com>
2023-03-02 21:30:03 +0000

    Put Factory seeding approach for BookReviewModel

--- database/factories/BookReviewModelFactory.php
+++ database/factories/BookReviewModelFactory.php
@@ -3,6 +3,8 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+use App\Models\BookModel;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\BookReviewModel>
@@ -16,8 +18,23 @@ class BookReviewModelFactory extends Factory
      */
     public function definition(): array
     {
+        $db_user_ids = User::pluck('id')->toArray();
+        $db_book_ids = BookModel::pluck('id')->toArray();
+
+        /**
+         * Faker Library: https://github.com/fzaninotto/Faker
+         */
         return [
-            //
+            'book_review_title' => fake()->sentence(),
+            'book_review_body' => fake()->paragraph(),
+            'book_review_rating' => fake()->numberBetween(1, 5),
+            'book_review_date' => fake()->date(),
+            'book_review_upvotes' => fake()->numberBetween(0, 100),
+            'book_review_downvotes' => fake()->numberBetween(0, 100),
+            'book_id' => fake()->randomElement($db_book_ids),
+            'user_id' => fake()->randomElement($db_user_ids),
         ];
+
+        // On line 34 and 35 - we are seeding random relationships from existing models - User & BookModel.
     }
 }
--- database/seeders/BookReviewTableSeeder.php
+++ database/seeders/BookReviewTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\BookReviewModel;
 
 class BookReviewTableSeeder extends Seeder
 {
@@ -12,6 +13,6 @@ class BookReviewTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        BookReviewModel::factory()->count(20)->create();
     }
 }
