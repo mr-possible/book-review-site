17405a731c0c4ac7fd73fd9dbfb3a85a66f031fb
mr-possible <sambhavd4@gmail.com>
2023-03-02 09:44:21 +0000

    Made Relevant Table Seeder Classes

---
+++ database/seeders/BookTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class BookTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
