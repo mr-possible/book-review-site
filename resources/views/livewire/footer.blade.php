e153e35049c09b581de8ef430c068a09ff5f2acf
mr-possible <sambhavd4@gmail.com>
2023-05-07 20:58:55 +0100

    Added images for front page

--- resources/views/livewire/footer.blade.php
+++ resources/views/livewire/footer.blade.php
@@ -1,3 +1,3 @@
-<footer class="footer has-background-dark has-text-white has-text-centered" is-fixed-bottom style="padding-top: 0.5rem; padding-bottom: 0.5rem; margin-top: 1rem;">
+<footer class="footer has-background-dark has-text-white has-text-centered" is-fixed-bottom style="padding-top: 0.5rem; padding-bottom: 0.2rem;">
   <p class="has-text-grey-light">&copy; Sambhav Dave | Student Id: 2267358 | CSCM48 | Swansea University 2023</p>
 </footer>
--- resources/views/livewire/homepage.blade.php
+++ resources/views/livewire/homepage.blade.php
@@ -3,50 +3,56 @@
 @section('title', 'Book Review Site')
 
 @section('content')
-<h1 class="title is-1 has-text-centered">Welcome <strong>{{ Auth::user()->name }}</strong> !</h1>
-<div class="dashboard-main-container columns is-multiline">
-  <div class="dashboard-main-card column is-one-third">
+<h1 class="title is-4 has-text-centered">Welcome <strong>{{ Auth::user()->name }}</strong> !</h1>
+<div class="columns">
+  <div class="column">
     <div class="card">
-      <header class="card-header has-text-centered">
-        <h5 class="card-header-title is-size-2">Search a Book</h5>
-      </header>
+      <div class="card-image">
+        <figure class="image is-4by3">
+          <img src="{{ asset('images/dashboard-search-book.jpg') }}" alt="Placeholder image">
+        </figure>
+      </div>
       <div class="card-content">
         <div class="content">
           Find and search for books in our extensive library collection.
         </div>
       </div>
-      <footer class="card-footer has-text-centered">
-        <a href="/searchbook" class="card-footer-item">Search</a>
+      <footer class="card-footer">
+        <a href="/searchbook" class="title is-4 card-footer-item">Search</a>
       </footer>
     </div>
   </div>
-  <div class="dashboard-main-card column is-one-third">
+  <div class="column">
     <div class="card">
-      <header class="card-header has-text-centered">
-        <h5 class="card-header-title is-size-2">Read a Review</h5>
-      </header>
+      <div class="card-image">
+        <figure class="image is-4by3">
+          <img src="{{ asset('images/dashboard-read-review.jpg') }}" alt="Placeholder image">
+        </figure>
+      </div>
       <div class="card-content">
         <div class="content">
           Read reviews of popular books and discover new titles.
         </div>
       </div>
-      <footer class="card-footer">
-        <a href="/readreview" class="card-footer-item">Read</a>
+      <footer class="card-footer has-text-centered">
+        <a href="/readreview" class="title is-4 card-footer-item">Read</a>
       </footer>
     </div>
   </div>
-  <div class="dashboard-main-card column is-one-third">
+  <div class="column">
     <div class="card">
-      <header class="card-header">
-        <h5 class="card-header-title is-size-2">Submit a Review</h5>
-      </header>
+      <div class="card-image">
+        <figure class="image is-4by3">
+          <img src="{{ asset('images/dashboard-submit-review.jpg') }}" alt="Placeholder image">
+        </figure>
+      </div>
       <div class="card-content">
         <div class="content">
-          Share your thoughts and contribute to our community of book lovers.
+          Share your thoughts and contribute to community of book lovers.
         </div>
       </div>
-      <footer class="card-footer">
-        <a href="/submitreview" class="card-footer-item">Submit</a>
+      <footer class="card-footer has-text-centered">
+        <a href="/submitreview" class="title is-4 card-footer-item">Submit</a>
       </footer>
     </div>
   </div>
